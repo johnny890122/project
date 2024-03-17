cb0581a747786939ce5bd8dd6ec5e0d92b445064
Xiang Xian, Zhang <j087061439@gmail.com>
2024-03-17 17:40:27 +0800

    deploy test

--- project/settings.py
+++ project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-8k2sdey^3l-z3@!@*6oumiq6n(2n$4&bp8ln2$=#ufg$3h3dv='
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['127.0.0.1', '<site_name>.herokuapp.com']
 
